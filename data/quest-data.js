ffadb6dd5c047473033d720fb49678f19a8b874e
sethripman <sethripman@gmail.com>
2019-10-18 10:18:56 -0700

    adding quests

--- data/quest-data.js
+++ data/quest-data.js
@@ -13,7 +13,7 @@ const fleet = {
     `,
     choices: [{
         id: 'flee',
-        description: 'Leave them to the hungry Darkstar, and continue on your way',
+        description: 'Leave them to the hungry Darkstar, and continue on your way.',
         result: `
             Only a fool risks the living for the dead. A word to the Navigator, and the Diamondclad remains on its course and abandons the treasure fleet to the Darkstar's maw. As you accelerate and the stars slide past, the fleet twinkles and disappears like a coin dropped into murky water. The Saint of Sorrows rests a hand against the viewport and sighs, but the Traveling Saint is already laughing and talking excitedly about your next destination. That night, you lie awake, imagining the tribute that was so important to a planet that they sent it across the Great Empty, and yet so meaningless to your mission you could not spare a moment and a scrap of fuel to satisfy your curiosity. Or was it?
         `,
@@ -23,12 +23,9 @@ const fleet = {
         trueHeirFound: 0,
     }, {
         id: 'tow',
-        description: 'Fiiiiiggghhhttt!',
+        description: 'There may be survivors sealed in mirror prisons. Tow the fleet to safety, no matter the cost.',
         result: `
-            Brandishing your sword you let out a warrior's cry and charge into the monsters
-            hacking and slashing. Before long you stand panting gazing across the bodies of
-            your vanquished foes. The bad news is you take 30 hp damage. The good news is you
-            find 50 gold.
+            The Diamondclad's engines heave with the strain of towing the treasure fleet free of the gravity well. The Waif sits by the engines and rests her metal hand against the wall, eyes closed, mouth moving slowly as she murmurs reassurance to the ancient vessel. You find no survivors amongst the great treasures of the lost world, but the other members of the crew look upon you with respect. That night, the Grand Dragon Admiral yields his seat at the high table to you. There is a moment of silence. Then the air fills with the clinking of silverware and friendly conversation.
         `,
         distillate: -2,
         honor: 2,
@@ -36,9 +33,9 @@ const fleet = {
         trueHeirFound: 0,
     }, {
         id: 'plunder',
-        description: '',
+        description: 'The Droneswarm can reach the fleet, costing far less fuel than sending the Diamondclad itself. Send in the Unmakers.',
         result: `
-            
+            The iridescent swarm streaks blackly towards the treasure fleet, their beetlelike appendages silhouetted against the crimson glare of the darkstar. By your side, the Lover of Games raises a crust of bread and breaks it in half with their long surgeon's fingers. They chew loudly. The Droneswarm reaches the fleet and they bloom with green plasma, shredding the vessels as smaller drones dart between the beams to scoop up priceless baubles from the wreckage. The work is quick and before the Lover of Games has finished eating, the drones have returned to their bays and the treasure is claimed. The Waif sits on a pile of gems, playing with a metal skull filigreed in gold. She raises it and looks into the eye sockets. What she sees there, she will not say to you. You leave her to her games and return to the Captain's nest.
         `,
         distillate: -1,
         honor: 0,
@@ -49,7 +46,7 @@ const fleet = {
 
 const saint = {
     id: 'saint',
-    title: 'A Problem Dragon',
+    title: 'The Traveling Saint in Search of Tombs has a Dinner Guest',
     map: {
         top: '57%',
         left: '67%'
@@ -58,23 +55,20 @@ const saint = {
     audio: 'dragon.wav',
     action: 'dragon-growl.aiff',
     description: `
-        You travel to a nearby village you have heard is being
-        terrorized by a dragon. Sure enough as you rent a room
-        in a local inn, you go outside and see the dragon about
-        to lay seige! What do you do?
+        That evening, as the lightbeetles scuttling along the Diamondclad's ceiling cease their brightness and open their shells to reveal gently glowing bioluminescence, a letter written on real paper in a calligrapher's hand is delivered to you by the Grand Dragon Admiral. It bears the Traveling Saint's stamp, and he requests your presence for a private dinner with a guest. The newcomer is a human girl, bundled in furs, with a hunted look in her eyes. The Traveling Saint draws you both into conversation, and his jovial laughter eases the silence between you two. Finally, after six courses of perfumed meat, vatgrown seedlings, and sauced fungus, he pulls you close and asks if you will swear to keep a secret, no matter the cost to yourself or the mission. The girl watches and waits for your answer.
     `,
     choices: [{
         id: 'polite',
-        description: '',
+        description: 'You were ordained. Bound by priests to return the Waif to her father, the Emperor. You cannot vow against this mission. Politely decline and resume conversation.',
         result: `
-            
+            The Traveling Saint in Search of Tombs nods sadly. He too has sacrified much in pursuit of an impossible goal. Dinner concludes with a tray of small cakes that unfold like lotus flowers into fractal shapes. The flavor is so exquisite that it brings tears to your eyes. Across the table, the girl is crying too. The Traveling Saint reaches out and takes her tiny hand in his. The moment passes, and you retire to your cell sated and curious. But you do not question your decision. The mission comes first. The Emperor's heir must be returned. All else is ashes before that noble goal.
         `,
         distillate: -1,
         honor: 1,
         heirFound: 0,
         trueHeirFound: 0,
     }, {
-        id: '',
+        id: 'vow',
         description: '',
         result: `
             
@@ -84,7 +78,7 @@ const saint = {
         heirFound: 0,
         trueHeirFound: 1,
     }, {
-        id: '',
+        id: 'leave',
         description: '',
         result: `
             
